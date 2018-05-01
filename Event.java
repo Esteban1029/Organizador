1510a452fe9442ddd224ed932e218f0ddfc54d81
Esteban1029 <38515038+Esteban1029@users.noreply.github.com>
2018-05-01 00:17:33 -0500

    Add files via upload

--- Event.java
+++ Event.java
@@ -1,5 +1,7 @@
+package proyecto;
 
-package calendar;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Event {
     private String name;
@@ -8,9 +10,9 @@ public class Event {
     private String place;
     private int importance;
 
-    public Event(String name, Date date, String description, String place, int importance) {
+    public Event(String name,String date, String description, String place, int importance) {
         this.name = name;
-        this.date = date;
+        setDate(date);
         this.description = description;
         this.place = place;
         this.importance = importance;
@@ -28,8 +30,64 @@ public class Event {
         return date;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setDate(String date) {
+        int year=0;
+        int month=0;
+        int day=0;
+        int hour=0;
+        Date verificar= new Date();
+        ArrayList<String> elementos =new ArrayList();
+        String aux[];
+
+            aux=date.split(" ");
+            for(String b:aux)
+            {
+                System.out.println(b);
+                if(!b.equals(""))
+                {
+                   elementos.add(b.trim());
+                }
+            }
+
+
+            for(String b:elementos)
+            {
+                if(elementos.size()==4)
+                {
+                    if(elementos.indexOf(b)==0)year=Integer.parseInt(b)-1900;
+                    if(elementos.indexOf(b)==1)month=Integer.parseInt(b)-1;
+                    if(elementos.indexOf(b)==2)day=Integer.parseInt(b);
+                    if(elementos.indexOf(b)==3)hour=Integer.parseInt(b);
+                   
+                   
+                    
+                }
+                else if(elementos.size()==3)
+                {
+                    if(elementos.indexOf(b)==0)year=Integer.parseInt(b)-1900;
+                    if(elementos.indexOf(b)==1)month=Integer.parseInt(b)-1;
+                    if(elementos.indexOf(b)==2)day=Integer.parseInt(b);
+                    
+                }
+                else
+                {
+                    System.out.println("Ingrese bien los datos");
+                }
+            }
+            
+            
+            if(year>verificar.getYear()||month>verificar.getMonth()||
+                    day>verificar.getDay()||(hour>verificar.getHours()&&
+                    hour!=0))
+            {
+                this.date= new Date(year,month,day,hour,0);
+            }
+            else
+            {
+                    System.out.println("Fecha incohrente");
+            }
+
+      
     }
 
     public String getDescription() {
@@ -55,5 +113,30 @@ public class Event {
     public void setImportance(int importance) {
         this.importance = importance;
     }
+    //
+    public String restTime()
+    {
+        System.out.println("Dias,horas,minutos");
+        // da el tiempo que falta en dias.
+        double time;
+        int days,hours,minutes;
+        String auxTime;
+        Date current = new Date();
+        time=(date.getTime()-current.getTime())/86400000d;
+        days=(int)time;
+        time-=Math.floor(time);
+        time*=24;
+        hours=(int)time;
+       
+        time-=Math.floor(time);
+        time*=60;
+        minutes=(int)time;
+        
+        auxTime=String.format("%d,%d,%d",
+                days,hours,minutes);
+        
+        return auxTime;
+    }
+    
     
 }
