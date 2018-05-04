cbc0ae20a20dbd62bc8e7b548ebd75e13057b9e4
Esteban <>
2018-05-04 18:44:53 -0500

    La clase Persona

---
+++ src/Data/Persona.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Esteban
+ */
+public class Persona implements Serializable{
+   
+   String nombre;
+   String correo;
+
+
+    public Persona(String nombre, String correo) {
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+}
