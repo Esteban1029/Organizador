8a18b2d8ed27eb35f4d01d22b0a1a944744c6b01
Andres Ardila <38542749+AndresArdila544@users.noreply.github.com>
2018-05-08 16:59:43 -0500

    Add files via upload

--- src/Data/Alarm.java
+++ src/Data/Alarm.java
@@ -3,12 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Data;
 
 /**
  *
- * @author Esteban
+ * @author Andres
  */
 public class Alarm {
+    String tipoAlarma;
+    Date diaNotificacion;
+
+    public String getTipoAlarma() {
+        return tipoAlarma;
+    }
+
+    public void setTipoAlarma(String tipoAlarma) {
+        this.tipoAlarma = tipoAlarma;
+    }
+
+    public Date getDiaNotificacion() {
+        return diaNotificacion;
+    }
+
+    public void setDiaNotificacion(Date diaNotificacion) {
+        this.diaNotificacion = diaNotificacion;
+    }
+
+    public Alarm(String tipoAlarma, Date diaNotificacion) {
+        this.tipoAlarma = tipoAlarma;
+        this.diaNotificacion = diaNotificacion;
+    }
     
 }
