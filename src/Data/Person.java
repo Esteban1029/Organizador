bf730d6de68e8d7f1e508b2f6d6fad0140004826
Andres Ardila <38542749+AndresArdila544@users.noreply.github.com>
2018-05-21 23:58:01 -0500

    Add files via upload

--- src/Data/Person.java
+++ src/Data/Person.java
@@ -52,6 +52,33 @@ public class Person implements Serializable{
         return true;
 
     }
+
+    public Person(String nombre, String correo, boolean user) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.user = user;
+    }
+
+    public Person(String nombre, String correo) {
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
     
     
     
